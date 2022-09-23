097f8eda9dc8641b05c3eb01a1e31f8314a3980e
Denzel Sapatin <dknsapatin@gmail.com>
2022-09-23 17:25:44 -0400

    Working on Business Right Feature Card

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import styles, { layout } from '../style';
+import { features } from '../constants';
+import Button from './Button';
 
-const Business = () => {
-  return <div>Business</div>;
-};
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index !== features.length - 1 ? 'mb-6' : 'mb-0'
+    } feature-card`}
+  >
+    <div>
+      <img src={icon} alt='icon' className='w-[5%] h-[50%] object-contain' />
+      <p className={`text-semibold text-white`}>{title}</p>
+      <p className={`${styles.paragraph}`}>{content}</p>
+    </div>
+  </div>
+);
+
+const Business = () => (
+  <section id='features' className={layout.section}>
+    {/* Left side Business */}
+    <div className={layout.section}>
+      <h2 className={`${styles.heading2}`}>
+        You do the business, <br className='sm:block hidden' /> we'll handle the
+        money
+      </h2>
+      <p className={`${styles.paragraph} text-white max-w-[400px] mt-5`}>
+        With the right credit card, you can improve financial life by building
+        credit, earning rewards and saving money
+      </p>
+      {/* Get Started Button */}
+      <Button styles='mt-10' />
+    </div>
+
+    {/* Right side Business */}
+    <div className={`${layout.sectionImg} flex-col`}>
+      {features.map((feature, index) => (
+        <FeatureCard key={feature.id} {...feature} index={feature.index} />
+      ))}
+    </div>
+  </section>
+);
 
 export default Business;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      type='button'
+      className={`py-4 py-6 font-poppins bg-blue-gradient font-medium text-primary outline-none ${styles}
+`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
