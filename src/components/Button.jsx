a5b51c71b04ab1ddd47136f8240c3cc95de12f13
Denzel Sapatin <dknsapatin@gmail.com>
2022-09-27 12:19:33 -0400

    CardDeal Section Component Complete

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type='button'
-      className={`py-4 px-6 font-poppins bg-blue-gradient font-poppins font-medium text-primary text-[18px  ] outline-none ${styles}`}
+      className={`py-4 px-6 font-poppins bg-blue-gradient font-poppins font-medium text-primary text-[18px] outline-none ${styles} rounded-[10px]`}
     >
       Get Started
     </button>
